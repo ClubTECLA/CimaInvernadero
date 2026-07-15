1c7fd8f33eadbd67d6f08edd4873f117f6d9b000
Lalex1611 <lalex1611@gmail.com>
2026-07-15 00:38:29 -0700

    feat/diseño: display de dispositivo seleccionado creado

--- frontend/src/components/Dispositivos/Dispositivos.js
+++ frontend/src/components/Dispositivos/Dispositivos.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+
+import defaultImg from "../../Assets/dispositivo-default.png";
 
 const estadoClases = {
   OPERATIVO: "estado-operativo",
@@ -35,6 +38,7 @@ function Dispositivos() {
                 monitoreo y control del invernadero
               </p>
             </Col>
+            {/* TODO: Agregar funcionalidad de boton */}
             <Col xs="auto" className="text-end">
               <button>+ Registrar dispositivo</button>
             </Col>
@@ -42,12 +46,42 @@ function Dispositivos() {
         </Container>
         <Container className="actual-display dispositivos-section card-component">
           {display ? (
-            <div>
-              <span className={`estado-badge ${estadoClases[display?.estado]}`}>
-                {display?.estado}
-              </span>
-              <h2>{display?.nombre}</h2>
-            </div>
+            <Row>
+              <Col md={2}>
+                <img
+                  src={display?.image_path || defaultImg}
+                  alt={display?.nombre}
+                  className="dispositivo-img"
+                />
+              </Col>
+              <Col>
+                {" "}
+                <div className="actual-display-info">
+                  <span
+                    className={`estado-badge ${estadoClases[display?.estado]}`}
+                  >
+                    {display?.estado}
+                  </span>
+                  <h2>{display?.nombre}</h2>
+                  <p>{display?.descripcion}</p>
+                  <p>
+                    <strong>Ubicacion: </strong> {display?.zona}
+                  </p>
+                </div>
+              </Col>
+              <Col md="auto">
+                {/* TODO: Agregar funcionalidad de boton */}
+                <button className="dispositivo-button editar-button">
+                  <AiFillEdit />
+                  Editar
+                </button>
+                {/* TODO: Agregar funcionalidad de boton */}
+                <button className="dispositivo-button eliminar-button">
+                  <AiFillDelete />
+                  Eliminar
+                </button>
+              </Col>
+            </Row>
           ) : (
             <p>Cargando dispositivo...</p>
           )}
